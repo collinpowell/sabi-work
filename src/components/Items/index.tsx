07c4b435526ab2bd038e501b22d805c3461a212b
collinpowell <collinskrubu723@gmail.com>
2024-05-22 23:59:19 +0100

    Swiper Update

--- src/components/Items/index.tsx
+++ src/components/Items/index.tsx
@@ -1,7 +1,19 @@
 import Image from "next/image"
+import { useRef, useState } from "react";
 import { Box, Heading, Container } from "theme-ui";
+import { Swiper, SwiperSlide, useSwiper } from "swiper/react";
+import { Autoplay } from "swiper/modules";
+
+import "swiper/css";
+import "swiper/css/autoplay";
+import "swiper/swiper-bundle.css";
+
 
 const Items = () => {
+  const swiper = useSwiper();
+
+  const swiperRef = useRef<any>(null);
+
   return (
     <Box sx={{
       display: ['none', null, null, 'contents']
@@ -12,7 +24,8 @@ const Items = () => {
         }}>Buy quality & affordable second-hand household items
         </Heading>
       </Container>
-
+      <br />
+      <br />
       <Box sx={{
         width: '100%',
         h2: {
@@ -20,6 +33,8 @@ const Items = () => {
         }
 
       }}>
+
+
 
         <Box sx={{
           padding: '0px',
@@ -30,11 +45,75 @@ const Items = () => {
           gap: '20px',
           img: {
             height: '700px',
+            width:'100%',
+            borderRadius:'10px',
+            objectFit:'cover'
           }
         }}>
-          <Image src="/assets/Washing machine.png" alt="img" width={600} height={200} />
-          <Image src="/assets/AC.png" alt="img" width={600} height={200} />
-          <Image src="/assets/TV.png" alt="img" width={600} height={200} />
+          <Swiper
+            spaceBetween={0}
+            centeredSlides={true}
+            centeredSlidesBounds
+            onSwiper={(swiper) => {
+              if (swiperRef.current) {
+                swiperRef.current = swiper;
+              }
+            }}
+            effect="slide"
+            preventClicks={false}
+            preventClicksPropagation={false}
+            speed={1200}
+            grabCursor={true}
+            //bounce={true}
+            ref={swiperRef}
+            loop={true}
+            autoplay={{
+              delay: 5000,
+              disableOnInteraction: false,
+              pauseOnMouseEnter: true,
+            }}
+            breakpoints={{
+              640: {
+                slidesPerView: 1,
+                spaceBetween: 20,
+              },
+              768: {
+                slidesPerView: 1.2,
+                spaceBetween: 5,
+              },
+              1024: {
+                slidesPerView: 2.2,
+                spaceBetween: 17,
+              },
+            }}
+            modules={[Autoplay]}
+          >
+
+            <SwiperSlide>
+              <Image src="/assets/items/1.jpg" alt="img" width={600} height={200} />
+            </SwiperSlide>
+
+            <SwiperSlide>
+              <Image src="/assets/items/2.jpg" alt="img" width={600} height={200} />
+            </SwiperSlide>
+
+            <SwiperSlide>
+              <Image src="/assets/items/3.jpg" alt="img" width={600} height={200} />
+            </SwiperSlide>
+
+            <SwiperSlide>
+              <Image src="/assets/items/4.jpg" alt="img" width={600} height={200} />
+            </SwiperSlide>
+
+            <SwiperSlide>
+              <Image src="/assets/items/5.jpg" alt="img" width={600} height={200} />
+            </SwiperSlide>
+
+            <SwiperSlide>
+              <Image src="/assets/items/6.jpg" alt="img" width={600} height={200} />
+            </SwiperSlide>
+
+          </Swiper>
 
         </Box>
       </Box>
