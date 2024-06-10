14bc3be8c6a132511a6959234fb5c5d7d9c31782
collinpowell <collinskrubu723@gmail.com>
2024-06-10 13:38:07 +0100

    GAID UPDATE

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,7 +6,7 @@ import type { AppProps } from "next/app";
 import { Toaster } from '@/components/Toaster';
 import { DefaultSeo } from 'next-seo';
 import SEO from '@/config/next-seo'
-import { GoogleTagManager } from '@next/third-parties/google'
+import { GoogleAnalytics } from '@next/third-parties/google'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -16,7 +16,7 @@ export default function App({ Component, pageProps }: AppProps) {
         <Component {...pageProps} />;
         <Toaster />
       </ThemeUIProvider>
-      <GoogleTagManager gtmId={'G-R8GXC1VL09'} />
+      <GoogleAnalytics gaId={'G-R8GXC1VL09'} />
     </>
 
   );
